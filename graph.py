be05894877d175ba5d4d49aafddfbb07723d3065
Vandana Mathi <67183853+VandanaMathi@users.noreply.github.com>
2023-05-05 23:05:04 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,14 @@
+import chart_studio.plotly
+import plotly.graph_objs as go
+from chart_studio.plotly import plot, iplot
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import networkx as nx 
+#matplotlib inline 
+from IPython.display import display
+from PIL import Image
+
+
+path=('C:/Users/Loura Shiny/Desktop/SIN/Project/marvel_image.png')
+display(Image.open(path))
